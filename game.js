22786353def10db04c017b3865f700b9e836a939
Cody Jeffries <Uwriyel@Codys-MacBook-Pro.local>
2018-11-23 12:44:23 -0800

    working water function

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 /*
 	File: Frupal.html
-	Authors: Henderson Hummel, James Hiebert,
+	Authors: Henderson Hummel, James Hiebert, Cody Jeffries
 	Date 10/27/2018
 
 	Purpose: display the game to user.
@@ -13,21 +13,107 @@
 
 const MAX = 20;
 const MAPSIZE = 5; //Distance from the player that tiles should be drawn
+/*var gameCanvas = {
+	width: 400, 
+	height: 400,
+};*/
+
+/*var canvas;
+var canvasContext;*/
+
+var canvas = document.getElementById('canvas');
+var canvasContext = canvas.getContext('2d');
+
+canvas.width = 400;
+canvas.height = 400;
+var leftX = 0;
+var topY = 0;
+const tileWidth = 20;
+const tileHeight = 20;
+
+function drawEverything() {
+	colorRect(0, 0, canvas.width, canvas.height, 'white')
+	drawMeadow();
+	drawForest();
+	drawWater();
+	drawWall();
+	drawBog();
+	drawSwap();
+}
+function drawMeadow() {
+	canvasContext.fillStyle = 'lawngreen';
+	canvasContext.fillRect(leftX, topY, tileWidth, tileHeight);
+	topY += tileWidth;
+	if (topY >= tileWidth) {
+		leftX += tileHeight;
+		topY = 0;
+	}
+}
+function drawForest() {
+	canvasContext.fillStyle = 'forestgreen';
+	canvasContext.fillRect(leftX, topY, tileWidth, tileHeight);
+	topY += tileWidth;
+	if (topY >= tileWidth) {
+		leftX += tileHeight;
+		topY = 0;
+	}
+}
+function drawWater() {
+	canvasContext.fillStyle = 'aqua';
+	canvasContext.fillRect(leftX, topY, tileWidth, tileHeight);
+	topY += tileWidth;
+	if (topY >= tileWidth) {
+		leftX += tileHeight;
+		topY = 0;
+	}
+}
+function drawWall() {
+	canvasContext.fillStyle = 'gainsboro';
+	canvasContext.fillRect(leftX, topY, tileWidth, tileHeight);
+	topY += tileWidth;
+	if (topY >= tileWidth) {
+		leftX += tileHeight;
+		topY = 0;
+	}
+}
+function drawBog() {
+	canvasContext.fillStyle = 'darkolivegreen';
+	canvasContext.fillRect(leftX, topY, tileWidth, tileHeight);
+	topY += tileWidth;
+	if (topY >= tileWidth) {
+		leftX += tileHeight;
+		topY = 0;
+	}
+}
+function drawSwap() {
+	canvasContext.fillStyle = 'olive';
+	canvasContext.fillRect(leftX, topY, tileWidth, tileHeight);
+	topY += tileWidth;
+	if (topY >= tileWidth) {
+		leftX += tileHeight;
+		topY = 0;
+	}
+}
+function colorRect(leftX, topY, width, height, drawColor) {
+	canvasContext.fillStyle = drawColor;
+	canvasContext.fillRect(leftX, topY, width, height);
+
+}
 var game = {
-	x_coord : 0,
-	y_coord : 0,
+	x_coord: 0,
+	y_coord: 0,
 	whiffles: 10000,
-	energy : 100,
+	energy: 100,
 
 	mapString: "",
-	mapMode : 0, //0 for mini-map, 1 for full map
-	inventoryCount : 0,
+	mapMode: 0, //0 for mini-map, 1 for full map
+	inventoryCount: 0,
 
-	jewels : {x: Math.round((Math.random() * 1000) % (MAX + 1)), y: Math.round((Math.random() * 1000) % (MAX + 1))},
+	jewels: { x: Math.round((Math.random() * 1000) % (MAX + 1)), y: Math.round((Math.random() * 1000) % (MAX + 1)) },
 
 	//This is our "main" function. It is run every time a button is pressed on our html page.
-	go : function(direc) {
-		switch(direc) {
+	go: function (direc) {
+		switch (direc) {
 			case 'n':
 				this.goNorth();
 				break;
@@ -56,20 +142,20 @@ var game = {
 		this.checkEnergy(); //checks energy and alerts user if energy < 0.
 		this.displayEnergy(); //adds energy to <div> in html.
 		this.displayMap(); //creates our map string, and displays to user.
-		
-		if(this.atJewels()){
+
+		if (this.atJewels()) {
 			alert("You've found the jewels! Use them wisely!");
 			this.gameOver();
 		}
 	},
 
 	//edits visibility for fog of war.
-	alterFlags : function() {
+	alterFlags: function () {
 		//stores the surround cells of the current hero coord (stores a 3X3 matrix of coords)
 
 		let x = this.x_coord; let y = this.y_coord;
-		let xminusone = x-1; let yminusone = y-1;
-		let xplusone = x+1; let yplusone = y+1;
+		let xminusone = x - 1; let yminusone = y - 1;
+		let xplusone = x + 1; let yplusone = y + 1;
 		//alert("xplusone is: " + xplusone);
 
 		//This section of the code will adjust the X,Y values whenever they're out of bounds, to keep us from trying to access an out-of-bound index 
@@ -114,35 +200,35 @@ var game = {
 				//If the coord doesn't exist in local storage, then a new cell will be made and pushed to local storage
 
 				//alert("This coord is NULL in local storage!");	
-				cell = surroundingCellsCoord[i]+",1,0,None";
+				cell = surroundingCellsCoord[i] + ",1,0,None";
 				//alert("This is what will be PUSHED to local storage: " + cell);
 				localStorage.setItem(surroundingCellsCoord[i], cell);
 			}
-			else {	
+			else {
 				//If the coord already has a cell in local storage, then this function will only modify the visibility flag, and then push the change to local storage again
 				cell = cell.split(",");
 				cell[2] = 1;
 				//alert("Current coord is NOT NULL in local storage (it exits)");
 				//alert("This is what will be PUSHED to local storage: " + cell);
-				localStorage.setItem(surroundingCellsCoord[i], cell);	
+				localStorage.setItem(surroundingCellsCoord[i], cell);
 
 			}
 		}
 	},
 
-	dispLoc : function() {
+	dispLoc: function () {
 		document.getElementById("loc").innerHTML = "Current Location:  " + this.x_coord + ',' + this.y_coord;
 	},
 
-	dispWhif : function() {
+	dispWhif: function () {
 		document.getElementById("whif").innerHTML = "Whiffles: " + this.whiffles;
 	},
 
 	//displays cell contents to user in html page. TODO: shouldn't be in final product.
-	checkLoc : function() {
+	checkLoc: function () {
 		var key = this.x_coord + ',' + this.y_coord;
 		let cellContents = localStorage.getItem(key);
-		if(cellContents != null) {
+		if (cellContents != null) {
 			cellContents = cellContents.split(',');
 			cellContents[2] = 1;
 			localStorage.setItem(key, cellContents);
@@ -156,114 +242,114 @@ var game = {
 			document.getElementById("cell").innerHTML = "Cell Details: " + newCell;
 		}
 	},
-	
-	addInventory : function() {
+
+	addInventory: function () {
 		let key = this.x_coord + ',' + this.y_coord; //dumb repeat but making it work
 		let cellContents = localStorage.getItem(key);
-		 cellContents = cellContents.split(',');
-		 
-		 switch (cellContents[4]){
+		cellContents = cellContents.split(',');
+
+		switch (cellContents[4]) {
 			case 'Hatchet':
-				if(this.promptPurchase("Hatchet", 50)) {
-					localStorage.setItem(inventory[0], localStorage.getItem(inventory[0])+1);
+				if (this.promptPurchase("Hatchet", 50)) {
+					localStorage.setItem(inventory[0], localStorage.getItem(inventory[0]) + 1);
 					document.getElementById("Hatchet").innerHTML = 'Hatchets: ' + localStorage.getItem(inventory[0]);
 					this.whiffles -= 50;
 				}
 				break;
 			case 'Hammer':
-				if(this.promptPurchase("Hammer", 50)) {
-					localStorage.setItem(inventory[1], localStorage.getItem(inventory[1])+1);
+				if (this.promptPurchase("Hammer", 50)) {
+					localStorage.setItem(inventory[1], localStorage.getItem(inventory[1]) + 1);
 					document.getElementById("Hammer").innerHTML = 'Hammers: ' + localStorage.getItem(inventory[1]);
 					this.whiffles -= 50;
 				}
 				break;
 			case 'Boat':
-				if(this.promptPurchase("Boat", 100)) {
-					localStorage.setItem(inventory[2], localStorage.getItem(inventory[2])+1);
+				if (this.promptPurchase("Boat", 100)) {
+					localStorage.setItem(inventory[2], localStorage.getItem(inventory[2]) + 1);
 					document.getElementById("Boat").innerHTML = 'Boats: ' + localStorage.getItem(inventory[2]);
 					this.whiffles -= 100;
 				}
 				break;
 			case 'Power Bar':
-				if(this.promptPurchase("Power Bar", 20)) {
+				if (this.promptPurchase("Power Bar", 20)) {
 					this.energy += 20;
 					this.whiffles -= 20;
 				}
 			case 'Axe':
-				if(this.promptPurchase("Axe", 50)) {
+				if (this.promptPurchase("Axe", 50)) {
 					localStorage.setItem(Axe, 1);
 					document.getElementById("Axe").innerHTML = 'Axes: ' + 1;
 				}
 				break;
 			case 'Chainsaw':
-				if(this.promptPurchase("Chainsaw", 50)) {
+				if (this.promptPurchase("Chainsaw", 50)) {
 					localStorage.setItem(Chainsaw, 1);
 					document.getElementById("Chainsaw").innerHTML = 'Chainsaws: ' + 1;
 				}
 				break;
 			case 'Chisel':
-				if(this.promptPurchase("Chisel", 50)) {
+				if (this.promptPurchase("Chisel", 50)) {
 					localStorage.setItem(Chisel, 1);
 					document.getElementById("Chisel").innerHTML = 'Chisels: ' + 1;
 				}
 				break;
 			case 'Sledge':
-				if(this.promptPurchase("Sledge", 50)) {
+				if (this.promptPurchase("Sledge", 50)) {
 					localStorage.setItem(Sledge, 1);
 					document.getElementById("Sledge").innerHTML = 'Sledges: ' + 1;
 				}
 				break;
 			case 'Machete':
-				if(this.promptPurchase("Machete", 50)) {
+				if (this.promptPurchase("Machete", 50)) {
 					localStorage.setItem(Machete, 1);
 					document.getElementById("Machete").innerHTML = 'Machetes: ' + 1;
 				}
 				break;
 			case 'Jackhammer':
-				if(this.promptPurchase("Jackhammer", 50)) {
+				if (this.promptPurchase("Jackhammer", 50)) {
 					localStorage.setItem(Jackhammer, 1);
 					document.getElementById("Jackhammer").innerHTML = 'Jackhammers: ' + 1;
 				}
 				break;
 			case 'Shear':
-				if(this.promptPurchase("Shear", 50)) {
+				if (this.promptPurchase("Shear", 50)) {
 					localStorage.setItem(Shear, 1);
 					document.getElementById("Shear").innerHTML = 'Shears: ' + 1;
 				}
 				break;
-		 }
-		 cellContents[4] = 'None';
-		 localStorage.setItem(key, cellContents);
+		}
+		cellContents[4] = 'None';
+		localStorage.setItem(key, cellContents);
 	},
 
-	promptPurchase : function(name, price) {                                           
+	promptPurchase: function (name, price) {
 		return confirm("You found a " + name + "!\n\n" + "Would you like to purchase it for " + price + " whiffles?");
 	},
 
-	atJewels : function() {
-		if((this.x_coord == this.jewels.x) && (this.y_coord == this.jewels.y))
+	atJewels: function () {
+		if ((this.x_coord == this.jewels.x) && (this.y_coord == this.jewels.y))
 			return true;
 		return false
 	},
 
-	goNorth : function() {
-		if(this.y_coord > 0) {
+	goNorth: function () {
+		if (this.y_coord > 0) {
 			--this.y_coord;
 		}
 		else {
 			this.y_coord = MAX - 1;
 		}
 	},
-	goWest : function() {
-		if(this.x_coord > 0) {
+	goWest: function () {
+		if (this.x_coord > 0) {
 			--this.x_coord;
 		}
 		else {
 			this.x_coord = MAX - 1;
 		}
 	},
-	goEast : function() {
-		if(this.x_coord < MAX - 1) {
+	goEast: function () {
+		if (this.x_coord < MAX - 1) {
 			++this.x_coord;
 		}
 		else {
@@ -271,8 +357,8 @@ var game = {
 		}
 	},
 
-	goSouth : function() {
-		if(this.y_coord < MAX - 1) {
+	goSouth: function () {
+		if (this.y_coord < MAX - 1) {
 			++this.y_coord;
 		}
 		else {
@@ -280,22 +366,21 @@ var game = {
 		}
 	},
 
-	displayEnergy : function() {
+	displayEnergy: function () {
 		document.getElementById("energy").innerHTML = "Energy: " + this.energy;
 	},
-	checkEnergy : function() {
+	checkEnergy: function () {
 		cellContents = localStorage.getItem(this.x_coord + ',' + this.y_coord);
-		if(cellContents != null)
-		{	
+		if (cellContents != null) {
 			var cell = cellContents.split(',');
-			if((cell[3] == 4) || (cell[3] == 5))
-				this.energy = this.energy-2;
+			if ((cell[3] == 4) || (cell[3] == 5))
+				this.energy = this.energy - 2;
 			else
 				--this.energy;
 		}
 		else
 			--this.energy;
-		if(this.energy < 1) {
+		if (this.energy < 1) {
 			var scream = new Audio("wilhelm.mp3");
 			scream.play();
 			alert("You are out of energy!");
@@ -303,67 +388,61 @@ var game = {
 		}
 	},
 
-	gameOver : function() {
-		window.location.href ="Frupalsplash.html";
+	gameOver: function () {
+		window.location.href = "Frupalsplash.html";
 	},
 
 	//0=meadow, 1=forest, 2=water, 3=wall, 4=bog, 5=swamp
-	displayMap : function()
-	{
-		var rowSize = (2*MAPSIZE)+1;
+	displayMap: function () {
+		var rowSize = (2 * MAPSIZE) + 1;
 		let tempMapString = "";
-			for(let i = 0; i < MAX; i++)
-			{
-				for(let j = 0; j < MAX; j++ )
-				{
-					let currCell = localStorage.getItem(j + ',' + i);
-					if( j == this.x_coord && i == this.y_coord)
-						tempMapString += 'C';
-					else if(currCell != null)
-					{
-						currCell = currCell.split(",");
+		for (let i = 0; i < MAX; i++) {
+			for (let j = 0; j < MAX; j++) {
+				let currCell = localStorage.getItem(j + ',' + i);
+				if (j == this.x_coord && i == this.y_coord)
+					tempMapString += 'C';
+				else if (currCell != null) {
+					currCell = currCell.split(",");
 
-						if(currCell[2] == '0')
-							tempMapString += 'X'
-						else switch(currCell[3])
-						{
-							case '0':
-								tempMapString += 'M';
-								break;
-							case '1':
-								tempMapString += 'F';
-								break;
-							case '2':
-								tempMapString += 'w';
-								break;
-							case '3':
-								tempMapString += 'W';
-								break;
-							case '4':
-								tempMapString += 'B';
-								break;
-							case '5':
-								tempMapString += 'S';
-								break;
-							default:
-								tempMapString += 'E'; // 'E' signifies some sort of error when checking the cell
-								break;
-						}
-					}
-					else
-					{
-						tempMapString += 'X';
+					if (currCell[2] == '0')
+						tempMapString += 'X'
+					else switch (currCell[3]) {
+						case '0':
+							tempMapString += 'M';
+							break;
+						case '1':
+							tempMapString += 'F';
+							break;
+						case '2':
+							tempMapString += 'w';
+							break;
+						case '3':
+							tempMapString += 'W';
+							break;
+						case '4':
+							tempMapString += 'B';
+							break;
+						case '5':
+							tempMapString += 'S';
+							break;
+						default:
+							tempMapString += 'E'; // 'E' signifies some sort of error when checking the cell
+							break;
 					}
 				}
-				tempMapString += "<br>";
+				else {
+					tempMapString += 'X';
+				}
 			}
+			tempMapString += "<br>";
+		}
 		this.mapString = tempMapString;
 		document.getElementById("map").innerHTML = this.mapString;
-		
+
 	},
-	changeMapMode : function()
-	{
-		if(this.mapMode == 0)
+
+	changeMapMode: function () {
+		if (this.mapMode == 0)
 			this.mapMode = 1;
 		else
 			this.mapMode = 0;
@@ -371,9 +450,16 @@ var game = {
 	},
 
 	//redirects us to the splash page if we didn't come from it.
-	checkFrom : function()
-	{
-		if(document.referrer.includes("Frupalsplash.html") == false)
+	checkFrom: function () {
+		if (document.referrer.includes("Frupalsplash.html") == false)
 			window.location.href = "Frupalsplash.html";
+	},
+
+	checkWater: function (xcoord, ycoord){
+
+		if(xcoord){
+			return;
+		}
 	}
 };
+
